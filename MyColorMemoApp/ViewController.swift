241f7e86e80d7c8723a69f8d6c70b0fe5748749c
taka-0303 <87518654+taka-0303@users.noreply.github.com>
2022-11-26 17:11:45 +0900

    Initial Commit

---
+++ MyColorMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyColorMemoApp
+//
+//  Created by 尊郁 on 2022/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
